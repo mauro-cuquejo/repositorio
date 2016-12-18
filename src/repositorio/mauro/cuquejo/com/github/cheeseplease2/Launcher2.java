fe6e2b73042105f65d113e2a62af161dfd21290c
Mauro Cuquejo <mauro.cuquejo@gmail.com>
2016-12-18 13:25:47 -0300

    Version 2 de cheesyPlease. Se modifican métodos por otros que vienen con el framework de libGdx

---
+++ src/repositorio/mauro/cuquejo/com/github/cheeseplease2/Launcher2.java
@@ -0,0 +1,10 @@
+package repositorio.mauro.cuquejo.com.github.cheeseplease2;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+public class Launcher2 {
+	
+	public static void main(String[] args) {
+		CheesePlease2 myProgram = new CheesePlease2();
+		LwjglApplication launcher = new LwjglApplication(myProgram);
+	}
+
+}
